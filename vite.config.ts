ac39c5508d0a42f16a0ec72b51de198f5a83bc9d
Alexandr Babac <bbkshow1987@gmail.com>
2024-03-15 09:52:21 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      features: resolve(__dirname, "src/features"),
+    },
+  },
+  plugins: [svgr({ svgrOptions: { icon: true } }), react()],
 });
